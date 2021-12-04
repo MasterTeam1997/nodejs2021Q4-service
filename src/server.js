fabfea3b4f651f0eee9ee779ff2031d1304585ac
MasterTeam1997 <musyaev@gmail.com>
2021-12-05 01:17:01 +0500

    reimplement using fastify

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
+app.register(require('./routes/user.route.js'))
+app.register(require('./routes/board.route.js'))
+app.register(require('./routes/task.route.js'))
+app.register(require('./routes/other.route.js'))
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+app.listen(PORT);
