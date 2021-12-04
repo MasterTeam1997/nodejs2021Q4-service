af37fd7ae48b4707cf31fc3a5c2a968492eb7ec0
MasterTeam1997 <musyaev@gmail.com>
2021-12-05 01:17:37 +0500

    add models

---
+++ src/resources/user/user.model.js
@@ -0,0 +1,17 @@
+const { v4: uuid } = require('uuid');
+
+class User {
+  constructor(name, login, password) {
+    this.id = uuid();
+    this.name = name;
+    this.login = login;
+    this.password = password;
+  }
+
+  static toResponse(user) {
+    const { id, name, login } = user;
+    return { id, name, login };
+  }
+}
+
+module.exports = User;
