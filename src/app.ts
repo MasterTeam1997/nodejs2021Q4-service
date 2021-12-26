501f76064041817540a3a209ecc9c5d1d6c7f685
MasterTeam1997 <musyaev@gmail.com>
2021-12-27 00:22:31 +0500

    add logger settings to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,22 @@
 import fastify from 'fastify';
+import config from './common/config';
 
-const app = fastify({ logger: true });
-export default app;
+const currentdate = new Date(); 
+const dateTime =  
+                 `${currentdate.getFullYear()  }-${  
+                 currentdate.getMonth()+1   }-${ 
+                 currentdate.getDate()  }_${
+                 currentdate.getHours()  }-${  
+                 currentdate.getMinutes()  }-${ 
+                 currentdate.getSeconds()}`;
+
+
+const filename = `logfile_${dateTime}`
+
+const app = fastify({
+    logger: {
+        level: config.LOG_LEVEL,
+        file: `./logs/${filename}`
+    }
+});
+export {app,filename}
