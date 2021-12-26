1d65ea48c1c6d217cb1a15c5f2710c4302443a2f
MasterTeam1997 <musyaev@gmail.com>
2021-12-27 00:23:20 +0500

    add logger activation

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,13 @@ import boardRoute from './routes/board.route'
 import taskRoute from './routes/task.route'
 
 import config from './common/config';
-import app from './app';
+import {app} from './app';
+import activateLogger from "./logger"
 
 app.register(userRoute);
 app.register(boardRoute);
 app.register(taskRoute);
 
 app.listen(config.PORT);
+activateLogger();
+// throw Error('Oops!')
